eaed3f44973ee8cf8480efa89c67ed09ddedcb52
ShikharSth <151653474+ShikharSth@users.noreply.github.com>
2024-05-23 19:40:14 +0545

    Create Array Creation.py

---
+++ Assignment_2/Array Creation.py
@@ -0,0 +1,12 @@
+# Create a 1-dimensional array of integers from 1 to 10.
+import numpy as np
+Q1 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+Q1
+
+# Create a 2-dimensional array with random float values of shape (3, 4).
+Q2 = np.random.rand(3,4)
+Q2
+
+# Create a 3-dimensional array of zeros with shape (2, 3, 4).
+Q3 = np.zeros((2,3,4))
+Q3
